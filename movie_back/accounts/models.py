f4e88ffbecff378d9d82509ada7a6e9c0e7c3910
정진권 <jjk8060@naver.com>
2020-06-15 23:47:45 +0900

    signup_debug

--- movie_back/accounts/models.py
+++ movie_back/accounts/models.py
@@ -6,5 +6,3 @@ from django.conf import settings
 class User(AbstractUser):
     followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followings")
     age = models.IntegerField()
-
-# /dg
