7e3b87c4eee12713417601ce678c637256981064
김윤진 <myung2ing@gmail.com>
2020-06-17 20:27:39 +0900

    Modify UI on score view

--- movie_front/src/views/community/ScoreView.vue
+++ movie_front/src/views/community/ScoreView.vue
@@ -1,15 +1,57 @@
 <template>
     <div class="p-5">
-        <p>{{ $route.query.movie.title }}</p>
-        <div class="form-group">
-            <label for="InputScore">Score</label>
-            <input type="text" class="form-control" id="InputScore" v-model="reviewData.score">
+        <p class="movie_title tag">{{ $route.query.movie.title }}</p>
+        <div class="d-flex justify-content-center mb-5">
+          <input type="checkbox" class="mr-3"><p class="watched_check">watched</p>
         </div>
-        <div class="form-group">
-            <label for="InputContent">CONTENT</label>
-            <input type="text" class="form-control" id="InputContent" v-model="reviewData.content" row="30">
+
+
+        <v-card
+          class="elevation-16 mx-auto"
+          width="300"
+        >
+          <v-card-title
+            class="headline"
+            primary-title
+          >
+            <p class="rate_text"><strong>당신이 본 영화를 평가해주세요</strong></p>
+          </v-card-title>
+          <v-card-text>
+            이곳에서 영화에 매긴 별점은 User Page의 My playlist에서 모두에게 보여질 수 있습니다
+            <div class="text-center mt-12">
+              <v-rating
+                v-model="rating"
+                color="yellow darken-3"
+                background-color="grey darken-1"
+                empty-icon="$ratingFull"
+                half-increments
+                hover
+              ></v-rating>
+            </div>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions class="justify-space-between">
+            <v-btn text>No Thanks</v-btn>
+            <v-btn
+              color="primary"
+              text
+            >
+              Rate Now
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <div class="score_form">
+          <div class="form-group">
+              <label for="InputScore">Score</label>
+              <input type="text" class="form-control" id="InputScore" v-model="reviewData.score">
+          </div>
+          <div class="form-group">
+              <label for="InputContent">CONTENT</label>
+              <input type="text" class="form-control" id="InputContent" v-model="reviewData.content" row="30">
+          </div>
+          <button type="submit" class="btn btn-success" @click="createReview($route.query.movie)">Create</button>
         </div>
-        <button type="submit" class="btn btn-success" @click="createReview($route.query.movie)">Create</button>
+
     </div>
 </template>
 
@@ -25,6 +67,7 @@ export default {
       reviewData: {
         score: null,
         content: '',
+        checked: true,
       },
     }
   },
@@ -93,6 +136,33 @@ export default {
 </script>
 
 <style>
+.movie_title {
+  color: seagreen;
+  width: 20rem;
+  border-radius: 0.8rem;
+  font-size: 1.7rem;
+  font-family: 'NeoDunggeunmo';
+  font-weight: bold;
+  font-style: normal;
+  text-align: center;
+  margin: auto;
+}
 
+.watched_check {
+  color: slategray;
+  text-align: center;
+  font-family: 'NeoDunggeunmo';
+  font-weight: bold;
+  font-style: normal;
+}
 
+.rate_text {
+  font-size: 1.1rem;
+}
+
+.score_form {
+  padding-top: 3rem;
+  width: 44rem;
+  margin: auto;
+}
 </style>
